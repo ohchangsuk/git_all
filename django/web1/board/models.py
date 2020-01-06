7a15836bf3b048bcf0ea8086960b319a7c0f3666
ohchangsuk <ckdtjr66@gmail.com>
2020-01-06 09:28:15 +0900

    1.6

--- django/web1/board/models.py
+++ django/web1/board/models.py
@@ -13,3 +13,14 @@ class table1(models.Model):
     hit     = models.IntegerField()
     regdate = models.DateField(auto_now_add=True)
     img     = models.BinaryField(null=True)
+
+
+class Table2(models.Model):
+    object = models.Manager() # vs오류 제거용
+
+    no     = models.AutoField(primary_key = True)#게시판 글 = 기본키 
+    name   = models.CharField(max_length=200)#글 제목
+    kor = models.IntegerField()
+    eng = models.IntegerField()
+    mat = models.IntegerField()
+    regdate = models.DateField(auto_now_add=True)
